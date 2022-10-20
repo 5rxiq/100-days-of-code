944fbcd9aa5c5e9cdf9f38876b394c56d67ea50e
wecada <wecada@amazon.com>
2022-10-20 16:41:29 -0600

    final iterations for the day

--- Day 010 - Functions with Outputs/10.5 Calculator Part 2.py
+++ Day 010 - Functions with Outputs/10.5 Calculator Part 2.py
@@ -1,5 +1,4 @@
 from art import logo
-print(logo)
 
 # add function
 def add(n1, n2):
@@ -25,26 +24,34 @@ operations = {
     "/": divide,
 }
 
-answer = ""
-keep_running = True
+def calc():
+    answer = ""
+    keep_running = True
+    print(logo)
 
-while keep_running is True:
-    if answer == "":
-        num1 = float(input("What's the first number?: "))
-    else:
-        num1 = float(answer)
+    while keep_running is True:
+        if answer == "":
+            num1 = float(input("What's the first number?: "))
+        else:
+            num1 = float(answer)
 
-    for symbol in operations:
-        print(symbol)
+        for symbol in operations:
+            print(symbol)
 
-    user_operation = input("Please choose a symbol from the list above: ")
+        user_operation = input("Please choose a symbol from the list above: ")
 
-    num2 = float(input("What's the second number?: "))
+        num2 = float(input("What's the second number?: "))
 
-    answer = operations[user_operation](n1=num1, n2=num2)
-    print(f"{num1} {user_operation} {num2} = {answer}")
+        answer = operations[user_operation](n1=num1, n2=num2)
+        print(f"{num1} {user_operation} {num2} = {answer}")
 
-    cont = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit. ")
+        cont = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to restart , or 'exit' to exit. ")
 
-    if cont == "n":
-        keep_running = False
+        if cont == "n":
+            keep_running = False
+            calc()
+        elif cont == "exit":
+            return
+            # keep_running = False
+
+calc()

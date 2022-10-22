e393262db1cb8ef31d18360ac18bd806a25f7394
wecadams-at-252229419755 <M4CY1nQqTIm3ljGzfYetkgkDCvZcOUeA/BkLWRKsl9U=>
2022-10-21 20:50:00 -0600

    changing 11 to 1 after first deal

--- Day 011 - Blackjack Capstone Project/11.2.py
+++ Day 011 - Blackjack Capstone Project/11.2.py
@@ -3,6 +3,8 @@ import random
 player = []
 computer = []
 keep_playing = True
+cont = None
+
 
 def deal():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -27,7 +29,9 @@ while keep_playing:
         for i in range(1):
             appending(player)
 
-# add in logic for 11 here.
+        if 11 in player:
+            player.remove(11)
+            player.append(1)
 
     for card in player:
         player_sum += card
@@ -35,7 +39,7 @@ while keep_playing:
     for card in computer:
         computer_sum += card
 
-    print(f"Your cards are: {player}; Your score is {player_sum}")
+    print(f"\nYour cards are: {player}; Your score is {player_sum}")
 
     if player_sum > 21:
         print(f"The dealer's cards are: {computer}; The dealer's total is {computer_sum}")
@@ -51,12 +55,12 @@ while keep_playing:
         keep_playing = False
     else:
         print(f"The dealer's first card is: {computer[0]}")
-        cont = input("Would you like to 'hit' or 'pass'?")
+        cont = input("Would you like to 'hit' or 'pass'? ")
 
     if cont == "pass":
-        print(f"Your cards are: {player}; Your total is {player_sum}")
+        print(f"\nYour cards are: {player}; Your total is {player_sum}")
         print(f"The dealer's cards are: {computer}; The dealer's total is {computer_sum}")
-        if player_sum > computer_sum and player_sum <= 21:
+        if computer_sum < player_sum <= 21:
             print("You won!")
         elif player_sum == computer_sum:
             print("You tied.")

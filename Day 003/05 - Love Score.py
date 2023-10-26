f11cb8ae42ebb74b907d96e710d801b7f3c2104c
wecadams-at-252229419755 <M4CY1nQqTIm3ljGzfYetkgkDCvZcOUeA/BkLWRKsl9U=>
2023-10-25 18:57:32 -0600

    love update

--- Day 003/05 - Love Score.py
+++ Day 003/05 - Love Score.py
@@ -73,10 +73,11 @@
 print("The Love Calculator is calculating your score...")
 name1 = input("What is name 1? ") # What is your name?
 name2 = input("What is name 2? ") # What is their name?
-names = name1 + name2
-names = names.lower()
 # 🚨 Don't change the code above 👆
 # Write your code below this line 👇
+
+names = name1 + name2
+names = names.lower()
 
 t = names.count("t")  
 r = names.count("r") 
@@ -95,7 +96,7 @@ final_score = int(final_score)
 print("The Love Calculator is calculating your score...")
 
 if final_score < 10 or final_score > 90:
-    print(f"You score is {final_score}, you go together like coke and mentos.")
+    print(f"Your score is {final_score}, you go together like coke and mentos.")
 
 elif final_score >= 40 and final_score <= 50:
     print(f"Your score is {final_score}, you are alright together.")

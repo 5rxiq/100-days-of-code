3aeae9c0c261cf1b8a5fe27c4ca1f79d1a8b1a09
wecadams-at-252229419755 <M4CY1nQqTIm3ljGzfYetkgkDCvZcOUeA/BkLWRKsl9U=>
2023-10-25 19:52:34 -0600

    final day 3. treasure hunt.

---
+++ Day 003/06 - Treasure Hunt.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure. \n") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+left_or_right = input("Would you like to go LEFT or RIGHT? ")
+
+if left_or_right.lower() == "left":
+    print("Congratulations! You survived.")
+    swim_or_wait = input("Would you like to SWIM or WAIT? ")
+    if swim_or_wait.lower() == "wait":
+        print("Congratulations! You survived.")
+        which_door = input("Which door do you want to go through? YELLOW, BLUE, or RED ")
+        if which_door.lower() == "blue":
+            print("You were eaten by a beast. Game over.")
+        elif which_door.lower() == "red":
+            print("You were burned by fire. Game over.")
+        elif which_door.lower() == "yellow":
+            print("You win!")
+        else:
+            print("Game Over.")
+    else:
+        print("You were attacked by a trout. Game over.")
+else:
+    print("You fell into a hole. Game over.")

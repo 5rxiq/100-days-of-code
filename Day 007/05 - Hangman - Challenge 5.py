a77acd6c80784dbc032145dac6bbb91504d81ca9
wecada <wecada@amazon.com>
2023-11-13 08:41:40 -0700

    day 7 complete

--- Day 007/05 - Hangman - Challenge 5.py
+++ Day 007/05 - Hangman - Challenge 5.py
@@ -11,6 +11,7 @@ word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
+guesses = []
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 print(hangman_art.logo)
@@ -28,21 +29,26 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in guesses:
+        print("You already guessed that letter.")
+    else:
+        guesses.append(guess)
 
-    #Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[position] = letter
+        #Check guessed letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
 
-    #Check if user is wrong.
-    if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-        lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You lose.")
+        #Check if user is wrong.
+        if guess not in chosen_word:
+            #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+            print(f"Sorry, {guess} is not in the word.")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("You lose.")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
